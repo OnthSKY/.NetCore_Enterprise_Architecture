b86365da2d829b373b97f3c63e06add36730f03d
SKY <onthsky@gmail.com>
2022-10-19 19:46:26 +0300

    New Entity has added according to Course

--- Entities/Concrete/Category.cs
+++ Entities/Concrete/Category.cs
@@ -1,6 +1,8 @@
-﻿namespace Entities.Concrete
+﻿using Entities.Abstract;
+
+namespace Entities.Concrete
 {
-    public class Category
+    public class Category : IEntity
     {
         public int CategoryId { get; set; }
         public string CategoryName { get; set; }
--- Entities/Concrete/Product.cs
+++ Entities/Concrete/Product.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Entities.Abstract;
 
 namespace Entities.Concrete
 {
-    public class Product
+    public class Product : IEntity
     {
         public int ProductId { get; set; }
         public int CategoryId { get; set; }
