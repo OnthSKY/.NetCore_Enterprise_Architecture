25a8f7ac10a2493bc56b0ab555e29f29653b1be0
SKY <onthsky@gmail.com>
2022-10-19 19:08:54 +0300

    Business Codes Has Added For Product

---
+++ Business/Abstract/IProductService.cs
@@ -0,0 +1,15 @@
+﻿using Entities.Concrete;
+using System.Collections.Generic;
+
+namespace Business.Abstract
+{
+    public interface IProductService
+    {
+        List<Product> GetAll();
+        List<Product> GetAllByCategory(int desiredCategoryId);
+
+        void Add(Product product);
+        void Delete(Product product);
+        void Update(Product product);
+    }
+}
