00eb8179449efdb6fe4eda768ed03befa2dd2051
SKY <onthsky@gmail.com>
2022-10-20 22:22:44 +0300

    New Codes has added to impelement aspect codes to WebAPI

--- Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
+++ Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -1,6 +1,9 @@
 ﻿using Autofac;
+using Autofac.Extras.DynamicProxy;
 using Business.Abstract;
 using Business.Concrete;
+using Castle.DynamicProxy;
+using Core.Utilities.Interceptors;
 using DataAccess.Abstract;
 using DataAccess.Concrete.EntityFramework;
 using System;
@@ -23,6 +26,14 @@ namespace Business.DependencyResolvers.Autofac
 
             builder.RegisterType<PersonelManager>().As<IPersonelService>().SingleInstance();
             builder.RegisterType<EfPersonelDal>().As<IPersonelDal>().SingleInstance();
+
+            var assembly = System.Reflection.Assembly.GetExecutingAssembly();
+
+            builder.RegisterAssemblyTypes(assembly).AsImplementedInterfaces()
+                .EnableInterfaceInterceptors(new ProxyGenerationOptions()
+                {
+                    Selector = new AspectInterceptorSelector()
+                }).SingleInstance();
         }
     }
 }
